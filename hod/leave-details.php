ab407774c0e7f8c37a1e2bd61f3f9f2d5f3df231
ashone77 <aswinvharidas@gmail.com>
2021-05-17 09:31:24 +0530

    Forwarding to principal complete. Leave details not working in Principal.

--- hod/leave-details.php
+++ hod/leave-details.php
@@ -22,25 +22,50 @@ $query->execute();
 // code for action taken on leave
 if(isset($_POST['update']))
 { 
-$did=intval($_GET['leaveid']);
-$description=$_POST['description'];
-$status=$_POST['status'];   
-date_default_timezone_set('Asia/Kolkata');
-$admremarkdate=date('Y-m-d G:i:s ', strtotime("now"));
-$dbh->query("INSERT into tblprincipal select * from tblleaves where id=$did");
-// updates the data in table principal - working code
-$sql="update tblprincipal set AdminRemark=:description,Status=:status,AdminRemarkDate=:admremarkdate where id=$did";
-// updates data also in table leaves - test code
-$sql_update="update tblleaves set AdminRemark=:description,Status=:status,AdminRemarkDate=:admremarkdate where id=$did";
+    switch ($_SESSION['alogin']){
+        case "hodcse":
+            $selectTbl = "tblleaves_cse";
+            break;
+        case "hodcivil":
+            $selectTbl = "tblleaves_civil";
+            break;
+        case "hodeee":
+            $selectTbl = "tblleaves_eee";
+            break;
+        case "hodme":
+            $selectTbl = "tblleaves_me";
+            break; 
+        case "hodec":
+            $selectTbl = "tblleaves_ece";
+            break;
+        case "hodash":
+            $selectTbl = "tblleaves_ash";
+            break;
+        default:
+            $selectTbl = "tblleaves";
+        
+    
+    }
+    
+    $did=intval($_GET['leaveid']);
+    $description=$_POST['description'];
+    $status=$_POST['status'];   
+    date_default_timezone_set('Asia/Kolkata');
+    $admremarkdate=date('Y-m-d G:i:s ', strtotime("now"));
+    $dbh->query("INSERT into tblprincipal select * from $selectTbl where id=$did");
+    // updates the data in table principal - working code
+    $sql="update tblprincipal set AdminRemark=:description,Status=:status,AdminRemarkDate=:admremarkdate where id=$did";
+    // updates data also in table leaves - test code
+    $sql_update="update $selectTbl set AdminRemark=:description,Status=:status,AdminRemarkDate=:admremarkdate where id=$did";
 
-$query = $dbh->prepare($sql);
-$query = $dbh->prepare($sql_update);
-$query->bindParam(':description',$description,PDO::PARAM_STR);
-$query->bindParam(':status',$status,PDO::PARAM_STR);
-$query->bindParam(':admremarkdate',$admremarkdate,PDO::PARAM_STR);
-// $query->bindParam(':did',$did,PDO::PARAM_STR); do not uncomment this line, page will crash
-$query->execute();
-$msg="Leave updated Successfully";
+    $query = $dbh->prepare($sql);
+    $query = $dbh->prepare($sql_update);
+    $query->bindParam(':description',$description,PDO::PARAM_STR);
+    $query->bindParam(':status',$status,PDO::PARAM_STR);
+    $query->bindParam(':admremarkdate',$admremarkdate,PDO::PARAM_STR);
+    // $query->bindParam(':did',$did,PDO::PARAM_STR); do not uncomment this line, page will crash
+    $query->execute();
+    $msg="Leave updated Successfully";
 }
 
 
