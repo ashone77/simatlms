0badeda5cccbcd4e7e7addaec937f6d8da31ef36
ashone77 <aswinvharidas@gmail.com>
2021-05-20 00:26:58 +0530

    HOD Leave Application page added - not functional

--- hod/includes/sidebar.php
+++ hod/includes/sidebar.php
@@ -45,24 +45,27 @@
                     </li> -->
 
    <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="leaves.php">All Leaves </a></li>
-                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
-                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
-                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
-       
-                            </ul>
-                        </div>
-                    </li>
+    <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+    <div class="collapsible-body">
+        <ul>
+            <li><a href="leaves.php">All Leaves </a></li>
+            <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
+            <li><a href="approvedleave-history.php">Approved Leaves</a></li>
+            <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
+            <li><a href="apply-leave.php">Apply Leave</a></li>
+
+        </ul>
+    </div>
+</li>
 
 
-  <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Change Password</a></li>
+  <li class="no-padding">
+    <a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Change Password</a>
+  </li>
 
-                        <li class="no-padding">
-                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
-                            </li>  
+<li class="no-padding">
+    <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
+</li>  
                  
 
                  
