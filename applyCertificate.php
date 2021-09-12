a2d763cfce54fc1fdbf5ec56baaff20f7a401543
ashone77 <aswinvharidas@gmail.com>
2021-09-12 21:48:43 +0530

    Apply certificate success

--- applyCertificate.php
+++ applyCertificate.php
@@ -4,6 +4,7 @@ error_reporting(0);
 include('includes/config.php');
 if(isset($_POST['update']))
 {
+
 $fname=$_POST["firstName"];
 $lname=$_POST["lastName"];
 $department=$_POST["Dept"];
@@ -15,7 +16,7 @@ $admssnNo=$_POST["Ano"];
 $bankName=$_POST["BName"];
 $bankBranch=$_POST["Branch"];
 
-$sql="INSERT INTO $bonafide_cert(FirstName,LastName,Department,Quota,AdmssnYear,CurrYear,LoanYear,AdmssnNo,BankName,BankBranch) VALUES(:fname,:lname,:department,:quota,:admssnYear,:currYear,:loanYear,:admssnNo,:bankName,:bankBranch)"; 
+$sql="INSERT INTO bonafide_cert(FirstName,LastName,Department,Quota,AdmssnYear,CurrYear,LoanYear,AdmssnNo,BankName,BankBranch,DocumentNumber,DateTime) VALUES(:fname,:lname,:department,:quota,:admssnYear,:currYear,:loanYear,:admssnNo,:bankName,:bankBranch,0,0)"; 
 $query= $dbh -> prepare($sql);
 $query-> bindParam(':fname', $fname, PDO::PARAM_STR);
 $query-> bindParam(':lname', $lname, PDO::PARAM_STR);
