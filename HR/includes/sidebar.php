49c3d0b31efba6bf9f3173841608c0c527032662
Aswin Haridas <aswinvharidas@gmail.com>
2021-09-15 22:52:50 +0530

    Added buttons for HR

--- HR/includes/sidebar.php
+++ HR/includes/sidebar.php
@@ -68,6 +68,9 @@
                         <li class="no-padding">
                                 <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Sign Out</a>
                             </li>  
+
+                            <li class="no-padding"><a class="waves-effect waves-grey" href="invoice-form-db.php"><i class="material-icons prefix">account_circle</i>Test : invoice-form-db</a></li>
+                            <li class="no-padding"><a class="waves-effect waves-grey" href="invoice-db.php"><i class="material-icons prefix">account_circle</i>Test :invoice-db</a></li>
                  
 
                  
