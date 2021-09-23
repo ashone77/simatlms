a9da87b896650d6a96567758b0f9a09d3f489f6c
Sruthy-M <sruthy.manithody@gmail.com>
2021-09-23 15:09:11 +0530

    login folder added

---
+++ StudentModule/loginsystem/includes/footer.php
@@ -0,0 +1,13 @@
+   <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; <?php echo date('Y');?></div>
+                            <div>
+                                <a href="https://phpgurukul.com/" target="_blank">PHPGurukul | Programming Blog</a>
+    
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
