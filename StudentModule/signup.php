af5a4b79d200477479f2eb7e039305b1b12cb87c
Dinoop7072 <dinoop7072@gmail.com>
2021-09-26 12:39:51 +0530

    Upated cert

--- StudentModule/signup.php
+++ StudentModule/signup.php
@@ -144,7 +144,7 @@ echo "Something went wrong. Please try again";
                 <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion" >
                     <li>&nbsp;</li>
                     <li class="no-padding"><a class="waves-effect waves-grey" href="./index.php"><i class="material-icons">account_box</i>Home</a></li>
-                    <li class="no-padding"><a class="waves-effect waves-grey" href="stindex.php"><i class="material-icons">account_box</i>Student Login</a></li>
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="index.php"><i class="material-icons">account_box</i>Student Login</a></li>
                   <!--  <li class="no-padding"><a class="waves-effect waves-grey" href="../simatlms/hod"><i class="material-icons">account_box</i>HOD Login</a></li>
                     <li class="no-padding"><a class="waves-effect waves-grey" href="../simatlms/principal"><i class="material-icons">account_box</i>Principal Login</a></li>
                     <li class="no-padding"><a class="waves-effect waves-grey" href="../simatlms/HR/"><i class="material-icons">account_box</i>HR Login</a></li>
