57f21f970bd16444d27092b4f650fe7ca17a855a
Dinoop7072 <dinoop7072@gmail.com>
2021-09-27 15:50:22 +0530

    added assets to principal pa

---
+++ principle_pa/cert.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/studentconfig.php');
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{
+
+
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <!-- Title -->
+        <link rel="shortcut icon" href="assets/images/logo.jpeg" type="image/ico" />
+        <title>Faculty | Leave History</title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Responsive Admin Dashboard Template" />
+        <meta name="keywords" content="admin,dashboard" />
+        <meta name="author" content="Steelcoders" />
+        
+        <!-- Styles -->
+        <link type="text/css" rel="stylesheet" href="assets/plugins/materialize/css/materialize.min.css"/>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="assets/plugins/material-preloader/css/materialPreloader.min.css" rel="stylesheet">
+        <link href="assets/plugins/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+            
+        <!-- Theme Styles -->
+        <link href="assets/css/alpha.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
+<style>
+        .errorWrap {
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #dd3d36;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+.succWrap{
+    padding: 10px;
+    margin: 0 0 20px 0;
+    background: #fff;
+    border-left: 4px solid #5cb85c;
+    -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+}
+        </style>
+    </head>
+    <body>
+       <?php include('includes/header.php');?>
+            
+       <?php include('includes/sidebar.php');?>
+            <main class="mn-inner">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="page-title">Certificate History</div>
+                    </div>
+                   
+                    <div class="col s12 m12 l12">
+                        <div class="card">
+                            <div class="card-content">
+                                <span class="card-title">Certificate History</span>
+                                <?php if($msg){?><div class="succWrap"><strong>SUCCESS</strong> : <?php echo htmlentities($msg); ?> </div><?php }?>
+                                <table id="example" class="display responsive-table ">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th width="200">Student Name</th>
+                                            <th width="200">Department</th>
+                                            <!-- <th>Admission Number</th> -->
+                                             <th>Posting Date</th>
+                                             <th width="200">Status</th>
+                                             <th></th>
+                                             <th align="center">Action</th>
+                                        </tr>
+                                    </thead>
+                                 
+                                    <tbody>
+
+
+
+
+
+
+
+
+
+
+<?php 
+$sql = "SELECT * FROM bonafide_cert ORDER BY DocumentNumber desc ";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{         
+      ?>  
+
+                                        <tr>
+                                            <td> <b><?php echo htmlentities($cnt);?></b></td>
+                                              <td><?php echo htmlentities($result->FirstName." ".$result->LastName);?></td>
+                                            <td><?php echo htmlentities($result->Department);?></td>
+                                            <td><?php echo htmlentities($result->PostingDate);?></td>
+                                                                       <td><?php $stats=$result->Status;
+if($stats==1){
+                                             ?>
+                                                 <span style="color: green">Approved</span>
+                                                 <?php } if($stats==2)  { ?>
+                                                <span style="color: darkorange">Office Staff Updated</span>
+                                                 <?php } if($stats==0)  { ?>
+ <span style="color: blue">Awaiting Verification</span>
+ <?php } ?></td>
+                                             
+
+          <td>
+             
+           <td><a href="cert-details.php?documentnumber=<?php echo htmlentities($result->DocumentNumber);?>" class="waves-effect waves-light btn blue m-b-xs"  > View Details</a></td>
+                                    </tr>
+                                         <?php $cnt++;} }?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+         
+        </div>
+        <div class="left-sidebar-hover"></div>
+        
+        <!-- Javascripts -->
+        <script src="assets/plugins/jquery/jquery-2.2.0.min.js"></script>
+        <script src="assets/plugins/materialize/js/materialize.min.js"></script>
+        <script src="assets/plugins/material-preloader/js/materialPreloader.min.js"></script>
+        <script src="assets/plugins/jquery-blockui/jquery.blockui.js"></script>
+        <script src="assets/plugins/datatables/js/jquery.dataTables.min.js"></script>
+        <script src="assets/js/alpha.min.js"></script>
+        <script src="assets/js/pages/table-data.js"></script>
+        
+    </body>
+</html>
+<?php } ?>
