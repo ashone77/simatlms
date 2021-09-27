2864a114281544ff1792da41e916598075532782
SREERAM MS <sreerammanatt@gmail.com>
2021-09-27 21:26:18 +0530

    off staff cert deleted

--- off_staff/includes/sidebar.php
+++ off_staff/includes/sidebar.php
@@ -27,19 +27,7 @@
                    
                   
 
-   <!-- <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Leave Management<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
-                        <div class="collapsible-body">
-                            <ul>
-                                <li><a href="leaves.php">All Leaves </a></li>
-                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
-                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
-                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
-       
-                            </ul>
-                        </div>
-                    </li> -->
-
+ 
 
   <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons">settings_input_svideo</i>Change Password</a></li>
 
